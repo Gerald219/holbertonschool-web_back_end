1959f221b6c3084f889e5d9692a93f77973f2dbc
Gerald Mulero <gdmasm@gmail.com>
2025-12-02 02:57:20 -0400

    returns the total seconds as a float

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -3,18 +3,18 @@
 
 import asyncio
 import time
+from typing import List
 
 async_comprehension = __import__("1-async_comprehension").async_comprehension
 
 
 async def measure_runtime() -> float:
-    """Run async_comprehension four times in parallel and return total time."""
+    """
+    Run async_comprehension four times in parallel
+    and return the total runtime in seconds.
+    """
     start = time.time()
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-    )
+    tasks: List = [async_comprehension() for _ in range(4)]
+    await asyncio.gather(*tasks)
     end = time.time()
     return end - start
